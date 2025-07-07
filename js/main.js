6f646addf775efff5f5c19fc0682ffb1b88e17c2
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-07-07 12:20:20 +0100

    modify version

--- js/main.js
+++ js/main.js
@@ -1,11 +1,51 @@
 
 
-const faqbuttons = document.querySelectorAll(".faqbutton");
-const faqAnswer = document.querySelectorAll(".hiddenfaq");
+(function(){
+    const rev = 400;
+    const revenue = document.getElementById('revenue');
+    const customer = document.getElementById('customer');
+    const growth = document.getElementById('growth');
+
+    let increase1 = 0;
+    let increase2 = 0;
+    let increase3 = 0;
+
+
+    const interval = setInterval(() => {
+            revenue.innerText = `${increase1}K;`
+
+                increase1++;
+                if (increase1 > 200) {
+                    clearInterval(interval);
+                }
+        }, 20);
+    const customerSat = setInterval(function () {
+            customer.innerText = `${increase2}K;`
+            increase2++;
+            if (increase2 > 10) {
+                    clearInterval(customerSat);
+                }
+    }, 200);
+
+    const customerGrowth = setInterval(() => {
+        growth.innerText = `${increase3}K`;
+        increase3++;
+        if(increase3 > 20) {
+            clearInterval(customerGrowth);
+        }
+
+    }, 200);
+
+
+
+
+    const faqbuttons = document.querySelectorAll(".faqbutton");
+    const faqAnswer = document.querySelectorAll(".hiddenfaq");
 
     for(let i=0; i < faqbuttons.length; i++){
         faqbuttons[i].addEventListener("click", function(event){
             event.preventDefault();
+            alert("youclick");
             const  faqAnswers = faqAnswer[i];
             if(!faqAnswers){
                 return;
@@ -21,4 +61,5 @@ const faqAnswer = document.querySelectorAll(".hiddenfaq");
                 faqAnswers.classList.add("hiddenfaq");
             }
         });
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    }   
+}());                                                                                                   
